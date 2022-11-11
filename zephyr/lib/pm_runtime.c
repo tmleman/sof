c06cd273cb192e29f1973d0e30e68f2553fe67da
Tomasz Leman <tomasz.m.leman@intel.com>
2022-11-11 01:58:49 +0100

    [DEBUG] ace: add logs in pm_runtime
    Stats about Idle and D0i3.
    
    Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>

--- zephyr/lib/pm_runtime.c
+++ zephyr/lib/pm_runtime.c
@@ -12,13 +12,21 @@
 #include <zephyr/pm/policy.h>
 #include <sof/ipc/driver.h>
 
-LOG_MODULE_REGISTER(power, CONFIG_SOF_LOG_LEVEL);
+LOG_MODULE_REGISTER(power, 3);
 
 /* 76cc9773-440c-4df9-95a8-72defe7796fc */
 DECLARE_SOF_UUID("power", power_uuid, 0x76cc9773, 0x440c, 0x4df9,
 		 0x95, 0xa8, 0x72, 0xde, 0xfe, 0x77, 0x96, 0xfc);
 
-DECLARE_TR_CTX(power_tr, SOF_UUID(power_uuid), LOG_LEVEL_INFO);
+DECLARE_TR_CTX(power_tr, SOF_UUID(power_uuid), 3);
+
+static uint32_t d0i3_count = 1;
+static int32_t d0i3_low_ticks = INT32_MAX;
+static int32_t d0i3_high_ticks = 1;
+
+static uint32_t idle_count = 1;
+static int32_t idle_low_ticks = INT32_MAX;
+static int32_t idle_high_ticks = 1;
 
 #if defined(CONFIG_PM_POLICY_CUSTOM)
 const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
@@ -59,10 +67,28 @@ const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
 			/* TODO: PM_STATE_RUNTIME_IDLE requires substates to be defined
 			 * to handle case with enabled PG andf disabled CG.
 			 */
+			tr_dbg(&power_tr, "cpu %x, ticks = %d, state counts = %d",
+			       cpu, ticks, num_cpu_states);
 			tr_dbg(&power_tr, "transition to state %x (min_residency = %u, exit_latency = %u)",
 			       state->state, min_residency, exit_latency);
+			d0i3_count += 1;
+			if (ticks < d0i3_low_ticks)
+				d0i3_low_ticks = ticks;
+
+			if (ticks > d0i3_high_ticks)
+				d0i3_high_ticks = ticks;
+
 			return state;
 		}
+	}
+
+	if (!pm_policy_state_lock_is_active(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES)) {
+		idle_count += 1;
+		if (ticks < idle_low_ticks)
+			idle_low_ticks = ticks;
+
+		if (ticks > idle_high_ticks)
+			idle_high_ticks = ticks;
 	}
 
 	return NULL;
@@ -74,8 +100,14 @@ void platform_pm_runtime_enable(uint32_t context, uint32_t index)
 	switch (context) {
 	case PM_RUNTIME_DSP:
 		pm_policy_state_lock_put(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES);
-		tr_dbg(&power_tr, "removing prevent on d0i3 (lock is active=%d)",
-		       pm_policy_state_lock_is_active(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES));
+		tr_info(&power_tr, "removing prevent on d0i3 (lock is active=%d)",
+			pm_policy_state_lock_is_active(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES));
+		d0i3_count = 0;
+		d0i3_low_ticks = INT32_MAX;
+		d0i3_high_ticks = 0;
+		idle_count = 0;
+		idle_low_ticks = INT32_MAX;
+		idle_high_ticks = 0;
 		break;
 	default:
 		break;
@@ -86,8 +118,12 @@ void platform_pm_runtime_disable(uint32_t context, uint32_t index)
 {
 	switch (context) {
 	case PM_RUNTIME_DSP:
-		tr_dbg(&power_tr, "putting prevent on d0i3");
-		pm_policy_state_lock_get(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES);
+		tr_info(&power_tr, "putting prevent on d0i3");
+			pm_policy_state_lock_get(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES);
+		tr_info(&power_tr, "PG stats[D0i3]: count %u, ticks: low = %d, high = %d",
+			d0i3_count, d0i3_low_ticks, d0i3_high_ticks);
+		tr_info(&power_tr, "PG stats[IDLE]: count %u, ticks: low = %d, high = %d",
+			idle_count, idle_low_ticks, idle_high_ticks);
 		break;
 	default:
 		break;
